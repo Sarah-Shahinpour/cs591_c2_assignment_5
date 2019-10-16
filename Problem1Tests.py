7442e59201600501c365f1002da6de51789364ff
Sohy Lee <sohylee@bu.edu>
2019-10-16 19:23:07 -0400

    Fix returnType comment

--- Problem1Tests.py
+++ Problem1Tests.py
@@ -6,7 +6,7 @@ problem1 = Problem1()
 class TestProblem1(unittest.TestCase):
     
     def test_returnType(self):
-        self.assertTrue(type(problem1.urlSplit("a://b/c")) is list, "test_returnType should return a list of strings")
+        self.assertTrue(type(problem1.urlSplit("a://b/c")) is list, "test_returnType should return a list")
 
     def test_inputWithAllComponents(self):
         self.assertEqual(problem1.urlSplit("https://www.google.com/some-path"), ["https", "www.google.com", "some-path"], "test_returnType should return all its components: http, www.google.com, some-path")
